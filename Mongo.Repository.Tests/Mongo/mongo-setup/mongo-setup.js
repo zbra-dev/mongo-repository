b557facc4a54a71cafaf47b936b86d2a1b68ebb7
Ale Cunha <ale@rcunha.net>
2022-04-15 11:09:49 -0700

    Improve scripts for setting up mongo replicaset

--- Mongo.Repository.Tests/Mongo/mongo-setup/mongo-setup.js
+++ Mongo.Repository.Tests/Mongo/mongo-setup/mongo-setup.js
@@ -1,20 +1,20 @@
 rsconf = {
-    _id : "rsmongo",
+    _id: "rsmongo",
     members: [
         {
             "_id": 0,
             "host": "mongodb1:27017",
-            "priority": 3
+            "priority": 1
         },
         {
             "_id": 1,
             "host": "mongodb2:27017",
-            "priority": 2
+            "priority": 0
         },
         {
             "_id": 2,
             "host": "mongodb3:27017",
-            "priority": 1
+            "priority": 0
         }
     ]
 }
--- Mongo.Repository.Tests/Mongo/mongo-setup/mongo-setup.sh
+++ Mongo.Repository.Tests/Mongo/mongo-setup/mongo-setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+/config/wait-for-mongo.sh mongodb1:27017 -- echo "mongodb1 ready"
+/config/wait-for-mongo.sh mongodb2:27017 -- echo "mongodb2 ready"
+/config/wait-for-mongo.sh mongodb3:27017 -- echo "mongodb3 ready"
+
 if [ ! -f /data/mongo-init.flag ]; then
     echo "Init replicaset"
     mongo mongodb://mongodb1:27017 mongo-setup.js
@@ -7,4 +11,12 @@ if [ ! -f /data/mongo-init.flag ]; then
 else
     echo "Replicaset already initialized"
 fi
+
+echo "Checking if database is ready...."
+
+until mongo --host mongodb1:27017 --eval "db.hello().isWritablePrimary" | tail -c+1 | grep "true"; do
+    echo "Waiting for Replica Set to Initialize Primary....."
+    sleep 1
+done
+
 echo "Replicaset is ready"
