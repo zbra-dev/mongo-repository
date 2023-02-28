e42bc187b35f3fb2d5854952443178529acdb1fa
Fillipe Rosini <hi@frosini.dev>
2023-02-28 13:36:59 -0300

    Added IReadOnlyList<T> support (#5)

---
+++ Mongo.Repository.Tests/ListTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+using ZBRA.Mongo.Repository.Impl;
+
+namespace ZBRA.Mongo.Repository.Tests
+{
+    [Collection("MongoCollection")]
+    public class ListTests
+    {
+        private readonly Repository<ListObj> repository;
+
+        public ListTests(MongoFixture fixture)
+        {
+            var mappings = new Mappings();
+            mappings.Entity<ListObj>()
+                .Infer(true)
+                .Build();
+            repository = new Repository<ListObj>(fixture.Client, fixture.GetDb(), mappings);
+            repository.Delete(repository.QueryAllAsync().Result.Entities);
+        }
+
+        [Fact]
+        public void TestList()
+        {
+            var listObj = new ListObj
+            {
+                List = new List<string> { "1" },
+                ReadOnlyList = new List<string> { "2" },
+            };
+            listObj.Id = repository.Insert(listObj);
+            listObj.Should().BeEquivalentTo(repository.FindById(listObj.Id).Value);
+        }
+
+        private class ListObj
+        {
+            public string Id { get; set; }
+            public IList<string> List { get; set; }
+            public IReadOnlyList<string> ReadOnlyList { get; set; }
+        }
+    }
+}
